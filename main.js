dc80bda1459c1539724d3041cd6594c1c563fb3d
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-09 23:44:31 +0000

    Should be working now

--- main.js
+++ main.js
@@ -4,26 +4,30 @@ import Navigo from "navigo";
 
 const router = new Navigo("/");
 
-const tpl = await getTemplate();
-const data = await getData();
+async function init() {
+  const tpl = await getTemplate();
+  const data = await getData();
 
-Handlebars.registerHelper("ifEq", function (v1, v2, options) {
-  if (v1 === v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
+  Handlebars.registerHelper("ifEq", function (v1, v2, options) {
+    if (v1 === v2) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  });
 
-router.on("/", async function () {
-  renderPage(tpl, data, "home");
-});
+  router.on("/", async function () {
+    renderPage(tpl, data, "home");
+  });
 
-router.on("/about", async function () {
-  renderPage(tpl, data, "about");
-});
+  router.on("/about", async function () {
+    renderPage(tpl, data, "about");
+  });
 
-router.on("/contact", async function () {
-  renderPage(tpl, data, "contact");
-});
+  router.on("/contact", async function () {
+    renderPage(tpl, data, "contact");
+  });
 
-router.resolve();
+  router.resolve();
+}
+
+init();
