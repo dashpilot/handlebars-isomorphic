364b886b3f30ba6cf770fcf2062b49dcdce2d888
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-10 14:04:41 +0000

    tweak

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ async function init() {
     "update",
     (e) => {
       console.log("update event fired");
-      console.log(e.detail);
+      renderPage(tpl, e.detail, e.detail.page);
     },
     false
   );
