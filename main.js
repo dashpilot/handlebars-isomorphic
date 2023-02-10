accac48734366bf680f1de16943657df7702dbc3
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-02-10 13:24:36 +0000

    test

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ import Navigo from "navigo";
 
 const router = new Navigo("/");
 
-function renderPage(tpl, data, page) {
+window.renderPage = function (tpl, data, page) {
   var template = Handlebars.compile(tpl);
   // data.entries = data.entries.filter((x) => x.page == page);
   data.page = page;
 
   document.querySelector("#app").innerHTML = template(data);
   console.log("template rendered");
-}
+};
 
 async function init() {
   const tpl = await getTemplate();
